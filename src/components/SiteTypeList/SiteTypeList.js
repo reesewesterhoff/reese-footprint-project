e13e2be433ca01de14994e7a7b125ba0dac0430a
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-11-09 15:28:34 -0600

    sites display on divs with button to select site

--- src/components/SiteTypeList/SiteTypeList.js
+++ src/components/SiteTypeList/SiteTypeList.js
@@ -5,8 +5,12 @@ import { connect } from 'react-redux';
 class SiteTypeList extends Component {
 
     componentDidMount() {
-        this.props.dispatch({type: 'FETCH_SITE_TYPES'});
-        
+        this.props.dispatch({type: 'FETCH_SITE_TYPES'});   
+    }
+
+    selectSite(site) {
+        console.log('clicked site', site);
+        // this.props.dispatch({type: 'SET_SELECTED_SITE', payload: site});
     }
 
     render() {
@@ -23,7 +27,14 @@ class SiteTypeList extends Component {
                     )}
                     
                 </select>
-                
+                {this.props.siteTypes.map(site => {
+                    return  <SiteTypeItem 
+                                site={site}
+                                key={site.id}
+                                selectSite={this.selectSite}
+                            />
+                    }      
+                )}
             </div>
         );
     }
