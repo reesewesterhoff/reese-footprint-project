4a532dde19d92e83d8bc9ae4c9a5d9bd24f5562b
Samuel Solberg <pauliesays65@gmail.com>
2018-11-09 16:50:59 -0600

    removed generator toggle from redux

--- src/redux/reducers/generatorReducer.js
+++ src/redux/reducers/generatorReducer.js
@@ -1,10 +1,5 @@
-let showGeneratorInputs = false;
-
 const generator = (state = {}, action) => {
     switch (action.type) {
-        case 'TOGGLE_GENERATOR':
-            showGeneratorInputs = !showGeneratorInputs;
-            return showGeneratorInputs;
         case 'ADD_GENERATOR':
             return {...action.payload};
         default:
