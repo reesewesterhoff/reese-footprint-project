c25db966f210c6f03f7f8ec073f50a0182624fc5
Samuel Solberg <pauliesays65@gmail.com>
2018-11-09 13:58:36 -0600

    generator checkbox and conditional rendering added

--- src/components/GeneratorForm/GeneratorForm.js
+++ src/components/GeneratorForm/GeneratorForm.js
@@ -1,9 +1,43 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class GeneratorForm extends Component{
+class GeneratorForm extends Component {
 
-    render(){
-        return <h1>Generator Form</h1>
+    state = {
+        hasGenerator: false,
+    }
+
+    handleGeneratorToggle = async event => {
+        await this.setState({
+            hasGenerator: !this.state.hasGenerator,
+        });
+        await console.log(this.state.hasGenerator);
+    }
+
+    render() {
+        return (
+            <div>
+                <h1>Generator Form</h1>
+
+                <h5>Do you have a generator(s)? Check box for 'yes'.</h5>
+                <input
+                    type="checkbox"
+                    value={this.state.hasGenerator}
+                    onClick={this.handleGeneratorToggle}
+                />
+
+                {this.state.hasGenerator ? (
+                    <React.Fragment>
+                        <input type="text" placeholder="Generator Size" />
+                        <br />
+                        <input type="text" placeholder="Monthly Fuel Cost" />
+                    </React.Fragment>
+                ) : (
+                        null
+                    )}
+
+            </div>
+
+        )
     }
 }
 
