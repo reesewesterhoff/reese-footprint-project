ea09c279be385f5adf1cf40197f74245d32c2be8
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-11-09 14:12:43 -0600

    add values and onchange function to inputs

--- src/components/AddSite/AddSite.js
+++ src/components/AddSite/AddSite.js
@@ -1,8 +1,8 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-class AddSite extends Component{
-    
-    state ={
+class AddSite extends Component {
+
+    state = {
         siteName: '',
         siteLocation: '',
         fundStartDate: '',
@@ -10,52 +10,50 @@ class AddSite extends Component{
         monthlyBudget: '',
     }
 
-    handleChange = (property) => (event) =>{
+    handleChange = (property) => (event) => {
         this.setState({
-        ...this.state,
-        [property]: event.target.value
-    })
-    }
-    
-    handleSubmit = (event) => {
-    event.preventDefault();
-    console.log(this.state);
+            ...this.state,
+            [property]: event.target.value
+        })
     }
 
-    render(){
+    handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(this.state);
+    }
+
+    render() {
         return (
 
             <div>
                 <h1>Add Site</h1>
 
                 <p>
-                This is a calculator that allows you to input your site information 
-                to do a cost-benefit comparison of using diesel energy generators 
-                to solar alternatives.
+                    This is a calculator that allows you to input your site information
+                    to do a cost-benefit comparison of using diesel energy generators
+                    to solar alternatives.
                 </p>
 
                 <form onSubmit={this.handleSubmit}>
                     <label>
-                        <input type="text" placeholder="Site Name" />
+                        <input type="text" placeholder="Site Name" value={this.state.siteName} onChange={this.handleChange('siteName')} />
                     </label>
                     <label>
-                      
-                        <input type="text" placeholder="Site Location" />
+                        <input type="text" placeholder="Site Location" value={this.state.siteLocation} onChange={this.handleChange('siteLocation')} />
                     </label>
                     <label>
-                       
-                        <input type="text"  placeholder="Funding Start Date" />
+                        <input type="text" placeholder="Funding Start Date" value={this.state.fundStartDate} onChange={this.handleChange('fundStartDate')} />
                     </label>
                     <label>
-                    
-                        <input type="text" placeholder="Funding End Date" />
+                        <input type="text" placeholder="Funding End Date" value={this.state.fundEndDate} onChange={this.handleChange('fundEndDate')} />
                     </label>
                     <label>
 
-                        <input type="text" placeholder="Monthly Budget" />
+                        <input type="text" placeholder="Monthly Budget" value={this.state.monthlyBudget} onChange={this.handleChange('monthlyBudget')} />
                     </label>
                     <input type="submit" value="Submit" />
                 </form>
+                
             </div>
 
         );
