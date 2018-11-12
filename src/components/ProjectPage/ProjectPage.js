4f220e5591c1ace79cd1346801fbabdb9e8ce1c7
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-11-12 15:00:40 -0600

    add card with sample data for view with styling

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -16,6 +16,16 @@ import './ProjectPage.css'
     return (
       <div >
         <h1 className="projectHeader">Project</h1>
+
+          <div className="card">
+            <img src="http://www.uhhospitals.org/~/media/UH/Images/locations/uh-chagrin-highlands-health-center.jpg" alt="Avatar" />
+            <div className="container">
+              <h4><b>Urgent Care Clinic</b></h4> 
+              <p>Est. Power Needs</p>
+              <p>10kWh/day</p>
+            </div>
+          </div>
+
       </div>
     );
   }
