1defda6a8ccd931c427b0ab8db9b5aaeed96ae2e
Nathan Salazar <nathan.salazar20@gmail.com>
2018-11-13 12:18:54 -0600

    dynamically set chart data

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react';
-import {Bar, Line} from 'react-chartjs-2';
+import { Bar, Line } from 'react-chartjs-2';
 import axios from 'axios';
-import {connect} from 'react-redux';
+import { connect } from 'react-redux';
 
 let start = '2014-12-30T18:06:17.762Z';
 let end = '2020-01-05T18:06:17.762Z';
 
 class Results extends Component {
 
-    state={
+    state = {
         messageToBeSent: {
             name: '',
             email: '',
@@ -18,7 +18,13 @@ class Results extends Component {
             // labels: [2015, 2016, 2017, 2018, 2019, 2020],
             datasets: [{
                 label: 'Time to cover initial investment',
-                data: [{x: start, y: this.props.state.selectedSite.total_price || 25000},{x: end, y: 0}], //these values will be set dynamically when user enters info
+                data: [{ //these values will be set dynamically when user enters info
+                    x: this.props.state.sites.length ? new Date(this.props.state.sites[0].fundStartDate) : start,
+                    y: this.props.state.selectedSite.total_price || 25000
+                }, {
+                    x: this.props.state.sites.length ? new Date(this.props.state.sites[0].fundEndDate) : end,
+                    y: 0
+                }],
                 backgroundColor: [
                     'rgba(54, 162, 235, 0.2)'
                 ],
@@ -26,13 +32,14 @@ class Results extends Component {
                     'rgba(54, 162, 235, 1)'
                 ],
                 borderWidth: 1,
-              
-            },{
+
+            }, {
                 label: 'Solar energy savings',
                 data: [{
-                    x: start, 
-                    y:1000},{
-                    x: end,
+                    x: this.props.state.sites.length ? this.props.state.sites[0].fundStartDate : start,
+                    y: 1000
+                }, {
+                    x: this.props.state.sites.length ? this.props.state.sites[0].fundEndDate : end,
                     y: 27000
                 }],
                 backgroundColor: [
@@ -42,12 +49,13 @@ class Results extends Component {
                     'green'
                 ],
                 borderWidth: 1
-            },{
+            }, {
                 label: 'Cost of Diesel',
                 data: [{
-                    x: start, 
-                    y:0},{
-                    x: end,
+                    x: this.props.state.sites.length ? this.props.state.sites[0].fundStartDate : start,
+                    y: 0
+                }, {
+                    x: this.props.state.sites.length ? this.props.state.sites[0].fundEndDate : end,
                     y: this.props.state.dieselCalculation || 30000
                 }],
                 backgroundColor: [
@@ -66,34 +74,28 @@ class Results extends Component {
                     unitStepSize: 1,
                     time: {
                         unit: 'year',
-                        max: '2020-01-05T18:06:17.762Z',
-                        min: '2014-12-30T18:06:17.762Z',
-                        suggestedMin: '2017-12-30T18:06:17.762Z',
+                        suggestedMax: '2020-01-05T18:06:17.762Z',
+                        suggestedMin: '2014-12-30T18:06:17.762Z',
                     },
                     distribution: 'linear'
                 }],
-                // yAxes: [{
-                //     ticks: {
-                //         suggestedMin: '2017-12-30T18:06:17.762Z'
-                //     }
-                // }]
             }
         }
     }
 
     handleChange = (property) => (event) => {
-        this.setState({ messageToBeSent: {...this.state.messageToBeSent, [property]: event.target.value} });
+        this.setState({ messageToBeSent: { ...this.state.messageToBeSent, [property]: event.target.value } });
     }
 
     handleSubmit = (event) => {
         event.preventDefault();
         console.log(`${this.state.messageToBeSent.name} is sending 
             from ${this.state.messageToBeSent.email} the following message: ${this.state.messageToBeSent.message}`);
-        axios.post('/email', {content: this.state.messageToBeSent, data: this.state.data.datasets.map(dataset=>dataset.data)}
-        ).then((response)=>{
-            console.log('Response is:',response.data);
-        }).catch((error)=>{
-            console.log('Error in POST:',error);
+        axios.post('/email', { content: this.state.messageToBeSent, data: this.state.data.datasets.map(dataset => dataset.data) }
+        ).then((response) => {
+            console.log('Response is:', response.data);
+        }).catch((error) => {
+            console.log('Error in POST:', error);
         })
     }
 
@@ -101,20 +103,21 @@ class Results extends Component {
     render() {
         return (<div>
             <h1>Results</h1>
-            <Line data={this.state.data} options={this.state.options}/>
+            <Line data={this.state.data} options={this.state.options} />
             <form onSubmit={this.handleSubmit}>
-                <input placeholder="Name" type="text" onChange={this.handleChange('name')}/>
-                <input placeholder="Email" type="text"  onChange={this.handleChange('email')}/>
-                <input placeholder="Message" type="text"  onChange={this.handleChange('message')}/>
+                <input placeholder="Name" type="text" onChange={this.handleChange('name')} />
+                <input placeholder="Email" type="text" onChange={this.handleChange('email')} />
+                <input placeholder="Message" type="text" onChange={this.handleChange('message')} />
                 <input type="submit" value="Contact the experts" />
             </form>
-            <pre>{JSON.stringify(this.props.state, null, 2)}</pre>
+            {/* <pre>{JSON.stringify(this.props.state, null, 2)}</pre>
+            <pre>{JSON.stringify(this.state, null, 2)}</pre> */}
         </div>)
     }
 }
 
 const mapStateToProps = (state) => {
-    return {state};
+    return { state };
 }
 
 export default connect(mapStateToProps)(Results);
