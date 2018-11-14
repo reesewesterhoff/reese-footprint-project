ed9091db4d5b3fab7eb083b1dd8bb390c6911f5f
Nathan Salazar <nathan.salazar20@gmail.com>
2018-11-14 16:29:25 -0600

    change chart values to better match input

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -15,60 +15,73 @@ class Results extends Component {
         name: '',
         email: '',
         message: '',
-        data: {
-            // labels: [2015, 2016, 2017, 2018, 2019, 2020],
-            datasets: [{
-                label: 'Time to cover initial investment',
-                data: [{ //these values will be set dynamically when user enters info
-                    x: this.props.state.sites.length ? new Date(this.props.state.sites[0].fundStartDate) : start,
-                    y: this.props.state.selectedSite.total_price || 25000
-                }, {
-                    x: this.props.state.sites.length ? new Date(this.props.state.sites[0].fundEndDate) : end,
-                    y: 0
-                }],
-                backgroundColor: [
-                    'rgba(54, 162, 235, 0.2)'
-                ],
-                borderColor: [
-                    'rgba(54, 162, 235, 1)'
-                ],
-
-                borderWidth: 1,
-
+        datasets: [{
+            label: 'Time to cover initial investment',
+            data: [{ //these values will be set dynamically when user enters info
+                x: this.props.state.sites.length ? new Date(this.props.state.sites[0].fundStartDate) : start,
+                y: this.props.state.selectedSite.total_price || 25000
             }, {
-                label: 'Solar energy savings',
-                data: [{
-                    x: this.props.state.sites.length ? this.props.state.sites[0].fundStartDate : start,
-                    y: 1000
-                }, {
-                    x: this.props.state.sites.length ? this.props.state.sites[0].fundEndDate : end,
-                    y: 27000
-                }],
-                backgroundColor: [
-                    'rgba(100, 100, 300, 0.4)'
-                ],
-                borderColor: [
-                    'green'
-                ],
-                borderWidth: 1
+                x: this.props.state.sites.length ? 
+                    new Date( Math.max(new Date(this.props.state.sites[0].fundEndDate).getTime(),
+                        new Date(this.props.state.sites[0].fundStartDate).getTime() + 
+                        this.props.state.selectedSite.total_price/(this.props.state.generator[0].monthlyCost)*2592000*1000)) : end,
+                y: 0
+            }],
+            backgroundColor: [
+                'rgba(54, 162, 235, 0.2)'
+            ],
+            borderColor: [
+                'rgba(54, 162, 235, 1)'
+            ],
+
+            borderWidth: 1,
+
+        }, {
+            label: 'Solar energy savings',
+            data: [{
+                x:  // get fundStartDate in milliseconds, then add the time it takes to recover initial investment
+                        new Date(this.props.state.sites[0].fundStartDate).getTime() 
+                    //2592000 is the number of seconds per month
+                    + this.props.state.selectedSite.total_price/(this.props.state.generator[0].monthlyCost)*2592000*1000 < 
+                        new Date(this.props.state.sites[0].fundEndDate).getTime() ?
+                    new Date(
+                        // get fundStartDate in milliseconds, then add the time it takes to recover initial investment
+                            new Date(this.props.state.sites[0].fundStartDate).getTime() 
+                        //2592000 is the number of seconds per month
+                        + this.props.state.selectedSite.total_price/(this.props.state.generator[0].monthlyCost)*2592000*1000 ) : new Date(this.props.state.sites[0].fundStartDate),
+                y: 0
             }, {
-                label: 'Cost of Diesel',
-                data: [{
-                    x: this.props.state.sites.length ? this.props.state.sites[0].fundStartDate : start,
-                    y: 0
-                }, {
-                    x: this.props.state.sites.length ? this.props.state.sites[0].fundEndDate : end,
-                    y: this.props.state.dieselCalculation.totalDieselCost || 35000
-                }],
-                backgroundColor: [
-                    'grey'
-                ],
-                borderColor: [
-                    'grey'
-                ],
-                borderWidth: 1
-            }]
-        },
+                x: this.props.state.sites.length ? this.props.state.sites[0].fundEndDate : end,
+                y:  new Date(this.props.state.sites[0].fundStartDate).getTime() 
+                //2592000 is the number of seconds per month
+                + this.props.state.selectedSite.total_price/(this.props.state.generator[0].monthlyCost)*2592000*1000 < 
+                    new Date(this.props.state.sites[0].fundEndDate).getTime() ? 
+                    this.props.state.dieselCalculation.totalDieselCost - this.props.state.selectedSite.total_price : 0
+            }],
+            backgroundColor: [
+                'rgba(100, 100, 300, 0.4)'
+            ],
+            borderColor: [
+                'green'
+            ],
+            borderWidth: 1
+        }, {
+            label: 'Cost of Diesel',
+            data: [{
+                x: this.props.state.sites.length ? this.props.state.sites[0].fundStartDate : start,
+                y: 0
+            }, {
+                x: this.props.state.sites.length ? this.props.state.sites[0].fundEndDate : end,
+                y: this.props.state.dieselCalculation.totalDieselCost || 35000
+            }],
+            backgroundColor: [
+                'grey'
+            ],
+            borderColor: [
+                'grey'
+            ],
+            borderWidth: 1
+        }],
         options: {
             scales: {
                 xAxes: [{
@@ -92,7 +105,8 @@ class Results extends Component {
 
     handleSubmit = (event) => {
         event.preventDefault();
-        axios.post('/email', { content: { name: this.state.name, email: this.state.email, message: this.state.message },
+        axios.post('/email', {
+            content: { name: this.state.name, email: this.state.email, message: this.state.message },
             siteName: this.props.state.sites[0].siteName,
             fundStartDate: this.props.state.sites[0].fundStartDate,
             fundEndDate: this.props.state.sites[0].fundEndDate,
@@ -100,7 +114,8 @@ class Results extends Component {
             generator: this.props.state.generator[0],
             selectedSite: this.props.state.selectedSite.type,
             totalDieselCost: this.props.state.dieselCalculation.totalDieselCost,
-            address: this.props.state.sites[0].address}
+            address: this.props.state.sites[0].address
+        }
         ).then((response) => {
             console.log('Response is:', response.data);
             this.setState({
@@ -118,8 +133,7 @@ class Results extends Component {
     render() {
         return (<div>
             <h2 className="heading">Results</h2>
-            <Line data={this.state.data} options={this.state.options} />
-
+            <Line data={{ datasets: this.state.datasets }} options={this.state.options} />
 
             <form onSubmit={this.handleSubmit}>
                 <TextField required placeholder="Name" type="text" onChange={this.handleChange('name')} value={this.state.name} />
