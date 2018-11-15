332a709a21aad5899ab56493814199359c80f0aa
Reese Westerhoff <reese.westerhoff@gmail.com>
2018-11-15 11:26:51 -0600

    icons displayed on site type item cards

--- src/components/SiteTypeItem/SiteTypeItem.js
+++ src/components/SiteTypeItem/SiteTypeItem.js
@@ -6,7 +6,6 @@ import CardActions from '@material-ui/core/CardActions';
 import CardContent from '@material-ui/core/CardContent';
 import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
 import Tooltip from '@material-ui/core/Tooltip';
 import Zoom from '@material-ui/core/Zoom';
 
@@ -15,13 +14,13 @@ const styles = {
     card: {
         marginTop: 15,
         width: 260,
-        height: 310,
+        height: 335,
         textAlign: 'center',
         backgroundColor: 'AliceBlue',
     },
     icon: {
         margin: 2,
-        fontSize: 60,
+        fontSize: 50,
         textAlign: 'center',
     },
     button: {
@@ -41,21 +40,23 @@ class SiteTypeItem extends Component {
                 <Tooltip TransitionComponent={Zoom} title={this.props.site.description} enterDelay={200} leaveDelay={100}>
                     <Card className={classes.card} >
                         <CardContent>
-                            {/* <Typography>
-                                <LocalHospitalIcon className={classes.icon} />
-                            </Typography> */}
+                            <Typography>
+                                {this.props.icon} 
+                            </Typography>
+                            {/* <br /> */}
                             <Typography variant="h6">
                                 <strong>{this.props.site.type}</strong>
                             </Typography>
+                            <br />
                             <div>
-                                <p>Power Needs: {this.props.site.power_need} kWh/day</p>
-                                <p>Panel Array: {this.props.site.solar_panel} kW</p>
-                                <p>Storage: {this.props.site.battery_bank} kWh</p>
+                                <p className="siteDescription">Power Needs: {this.props.site.power_need} kWh/day</p>
+                                <p className="siteDescription">Panel Array: {this.props.site.solar_panel} kW</p>
+                                <p className="siteDescription">Storage: {this.props.site.battery_bank} kWh</p>
                                 {
                                     this.props.site.generator ?
-                                        <p>Generator: {this.props.site.generator} kW</p>
+                                        <p className="siteDescription">Generator: {this.props.site.generator} kW</p>
                                         :
-                                        <p>Generator: None</p>
+                                        <p className="siteDescription">Generator: None</p>
                                 }
                             </div>
                         </CardContent>
--- src/components/SiteTypeList/SiteTypeList.js
+++ src/components/SiteTypeList/SiteTypeList.js
@@ -4,6 +4,14 @@ import { connect } from 'react-redux';
 import SiteTypeCategory from '../SiteTypeCategory/SiteTypeCategory';
 import PropTypes from 'prop-types';
 import { withStyles } from '@material-ui/core/styles';
+import LocalHospitalIcon from '@material-ui/icons/LocalHospital';
+import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
+import RestaurantIcon from '@material-ui/icons/Restaurant';
+import RouterIcon from '@material-ui/icons/Router';
+import WavesIcon from '@material-ui/icons/Waves';
+import SecurityIcon from '@material-ui/icons/Security';
+import HomeIcon from '@material-ui/icons/Home';
+import StoreIcon from '@material-ui/icons/Store';
 
 const styles = {
     card: {
@@ -12,24 +20,48 @@ const styles = {
         flexWrap: 'wrap',
         justifyContent: 'space-evenly',
         margin: 10,
-    }
+    },
 }
 
 
 class SiteTypeList extends Component {
 
-    componentDidMount() {
-       
+    state = {
+        icon: '',
     }
 
     selectSite = (site) => {
-        console.log('clicked site', site);
         this.props.dispatch({ type: 'SET_SELECTED_SITE', payload: site });
     }
 
-    selectSiteCategory = (category) => {
-        console.log(category);
-        this.props.dispatch({ type: 'FETCH_SITE_TYPES', payload: category });
+    selectSiteCategory = async (category) => {
+        await this.props.dispatch({ type: 'FETCH_SITE_TYPES', payload: category });
+        this.setState({
+            icon: this.chooseIcon(category),
+        });    
+    }
+
+    chooseIcon = (iconCategory) => {
+        switch (iconCategory) {
+            case 'Health':
+                return <LocalHospitalIcon />
+            case 'Water':
+                return <WavesIcon />
+            case 'Comms':
+                return <RouterIcon />
+            case 'Ops':
+                return <SecurityIcon />
+            case 'Shelter':
+                return <HomeIcon />
+            case 'Food':
+                return <RestaurantIcon />
+            case 'Admin':
+                return <BusinessCenterIcon />
+            case 'Logs':
+                return <StoreIcon />
+            default:
+                return null;
+        }
     }
 
     render() {
@@ -51,6 +83,7 @@ class SiteTypeList extends Component {
                             site={site}
                             key={site.id}
                             selectSite={this.selectSite}
+                            icon={this.state.icon}
                         />
                     }
                     )}
