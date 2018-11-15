6519fb58e92d62b759fc4009c8c1f0a9e76d2ca9
Nathan Salazar <nathan.salazar20@gmail.com>
2018-11-15 16:48:04 -0600

    move datasets to render

--- src/components/Results/Results.js
+++ src/components/Results/Results.js
@@ -16,7 +16,56 @@ class Results extends Component {
         name: '',
         email: '',
         message: '',
-        datasets: [{
+        options: {
+            scales: {
+                xAxes: [{
+                    type: 'time',
+                    unitStepSize: 1,
+                    time: {
+                        unit: 'year',
+                        suggestedMax: '2020-01-05T18:06:17.762Z',
+                        suggestedMin: '2014-12-30T18:06:17.762Z',
+                    },
+                    distribution: 'linear'
+                }],
+            }
+        }
+    }
+
+    handleChange = (property) => (event) => {
+        this.setState({ [property]: event.target.value });
+
+    }
+
+    handleSubmit = (event) => {
+        event.preventDefault();
+        axios.post('/email', {
+            content: { name: this.state.name, email: this.state.email, message: this.state.message },
+            siteName: this.props.sites[0].siteName,
+            fundStartDate: this.props.sites[0].fundStartDate,
+            fundEndDate: this.props.sites[0].fundEndDate,
+            location: this.props.sites[0].location,
+            generator: this.props.generator[0],
+            selectedSite: this.props.selectedSite.type,
+            totalDieselCost: this.props.dieselCalculation.totalDieselCost,
+            address: this.props.sites[0].address
+        }
+        ).then((response) => {
+            console.log('Response is:', response.data);
+            this.setState({
+                name: '',
+                email: '',
+                message: '',
+            });
+        }).catch((error) => {
+            console.log('Error in POST:', error);
+        })
+    }
+
+
+
+    render() {
+        const datasets = [{
             label: 'Time to cover initial investment',
             data: [{ //these values will be set dynamically when user enters info
                 x: this.props.sites.length ? new Date(this.props.sites[0].fundStartDate) : start,
@@ -68,63 +117,16 @@ class Results extends Component {
                 'grey'
             ],
             borderWidth: 1
-        }],
-        options: {
-            scales: {
-                xAxes: [{
-                    type: 'time',
-                    unitStepSize: 1,
-                    time: {
-                        unit: 'year',
-                        suggestedMax: '2020-01-05T18:06:17.762Z',
-                        suggestedMin: '2014-12-30T18:06:17.762Z',
-                    },
-                    distribution: 'linear'
-                }],
-            }
-        }
-    }
-
-    handleChange = (property) => (event) => {
-        this.setState({ [property]: event.target.value });
-
-    }
-
-    handleSubmit = (event) => {
-        event.preventDefault();
-        axios.post('/email', {
-            content: { name: this.state.name, email: this.state.email, message: this.state.message },
-            siteName: this.props.sites[0].siteName,
-            fundStartDate: this.props.sites[0].fundStartDate,
-            fundEndDate: this.props.sites[0].fundEndDate,
-            location: this.props.sites[0].location,
-            generator: this.props.generator[0],
-            selectedSite: this.props.selectedSite.type,
-            totalDieselCost: this.props.dieselCalculation.totalDieselCost,
-            address: this.props.sites[0].address
-        }
-        ).then((response) => {
-            console.log('Response is:', response.data);
-            this.setState({
-                name: '',
-                email: '',
-                message: '',
-            });
-        }).catch((error) => {
-            console.log('Error in POST:', error);
-        })
-    }
+        }]
 
 
-
-    render() {
         return (<div>
             <h2 className="heading">Results</h2>
-            <Line data={{ datasets: this.state.datasets }} options={this.state.options} />
-            <h3>Time to pay off: {this.props.dieselCalculation.timeToPayOff} months</h3>
+            <Line data={{ datasets: datasets }} options={this.state.options} />
+            <h3>Time to pay off: {parseInt(this.props.dieselCalculation.timeToPayOff)} months</h3>
             {this.props.dieselCalculation.payOffInTime ?
-                <h3>Total Savings: ${(this.props.dieselCalculation.totalDieselCost - this.props.selectedSite.total_price).toFixed(2)}</h3> :
-                <h3>Monthly budget needed: ${(this.props.selectedSite.total_price / this.props.dieselCalculation.timeline).toFixed(2)}</h3>}
+                <h3>Total Savings: ${parseInt(this.props.dieselCalculation.totalDieselCost - this.props.selectedSite.total_price)}</h3> :
+                <h3>Monthly budget needed: ${parseInt(this.props.selectedSite.total_price / this.props.dieselCalculation.timeline)}</h3>}
 
             <form onSubmit={this.handleSubmit}>
                 <TextField required placeholder="Name" type="text" onChange={this.handleChange('name')} value={this.state.name} />
@@ -132,7 +134,7 @@ class Results extends Component {
                 <TextField placeholder="Message" type="text" onChange={this.handleChange('message')} value={this.state.message} />
                 <input type="submit" value="Contact the experts" />
             </form>
-            {/* <pre>{JSON.stringify(this.props, null, 2)}</pre> */}
+            <pre>{JSON.stringify(this.props, null, 2)}</pre>
             {/* <pre>{JSON.stringify(this.state, null, 2)}</pre> */}
         </div>)
     }
