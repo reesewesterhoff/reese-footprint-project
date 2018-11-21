39ccf4e82abe93d408f6ff26c57cb9c6fcf176a9
Nicholas Manchanthasouk <nikmanchan@hotmail.com>
2018-11-21 15:58:13 -0600

    add marker to map on project view and select site functionality on marker click

--- src/components/ProjectPage/ProjectPage.js
+++ src/components/ProjectPage/ProjectPage.js
@@ -17,7 +17,8 @@ import SecurityIcon from '@material-ui/icons/Security';
 import HomeIcon from '@material-ui/icons/Home';
 import StoreIcon from '@material-ui/icons/Store';
 import './ProjectPage.css'
-import Map from '../Map/Map'
+import SitesMap from '../SitesMap/SitesMap'
+import SiteMarker from '../Marker/Marker'
 
 
 const styles = theme => ({
@@ -64,7 +65,7 @@ class ProjectPage extends Component {
     }
 
     selectSite = (index) => {
-        console.log(index);
+        console.log('index:', index);
         this.setState({
             currentIndex: index,
         })
@@ -128,20 +129,6 @@ class ProjectPage extends Component {
 
 
                         </div>}
-                             <Map
-                                googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZv9A4Vtnra6r04z9JnNk91zeXwX82O68&v=3.exp&libraries=geometry,drawing,places"
-                                loadingElement={<div style={{ height: `100%` }} />}
-                                containerElement={<div style={{ height: `80vmin`, width: '80vmin', margin: 'auto', }} />}
-                                mapElement={<div style={{ height: `100%` }} />}
-                                isMarkerShown={this.state.isMarkerShown}
-                                onMarkerClick={this.handleMarkerClick}
-                                handleToggleOpen={this.handleToggleOpen}
-                                isOpen={this.state.isOpen}
-                                onClick={this.handleClick}
-                                location={this.state.location}
-                                handleClick={this.handleClick}
-                                mapClicked={this.state.mapClicked}
-                            />
 
                     {!this.props.sitesByProject.length ? <h2>Add a site to get started!</h2> :
                     <div>
@@ -186,7 +173,27 @@ class ProjectPage extends Component {
                                     </CardActions>
                                 </Card>
                             )}
-                            
+                                <SitesMap
+                                    googleMapURL="https://maps.googleapis.com/maps/api/js?key=AIzaSyCZv9A4Vtnra6r04z9JnNk91zeXwX82O68&v=3.exp&libraries=geometry,drawing,places"
+                                    loadingElement={<div style={{ height: `100%` }} />}
+                                    containerElement={<div style={{ height: `80vmin`, width: '80vmin', marginLeft: '35vmin', }} />}
+                                    mapElement={<div style={{ height: `100%` }} />}
+                                    isMarkerShown={this.state.isMarkerShown}
+                                    onMarkerClick={this.handleMarkerClick}
+                                    handleToggleOpen={this.handleToggleOpen}
+                                    isOpen={this.state.isOpen}
+                                    onClick={this.handleClick}
+                                    location={this.state.location}
+                                    handleClick={this.handleClick}
+                                    mapClicked={this.state.mapClicked}
+                                    selectSite={this.selectSite}
+                                >
+                           
+
+                                </SitesMap>
+
+
+                                
                         </div>
                                 {this.state.currentIndex < 0 ? <h2 id="siteSelect">Choose a site!</h2> :
                                     <SavedSitePage
---
+++ src/components/SitesMap/SitesMap.js
@@ -0,0 +1,141 @@
+import React from 'react';
+import { connect } from 'react-redux';
+import { withScriptjs, withGoogleMap, GoogleMap } from 'react-google-maps';
+import { SearchBox } from 'react-google-maps/lib/components/places/SearchBox';
+import _ from 'lodash';
+import { Marker } from 'react-google-maps';
+import SiteMarker from '../Marker/Marker'
+/* global google */
+
+class SitesMap extends React.Component {
+  state = {
+    bounds: null,
+    center: {
+      lat: 40, lng: 45
+    },
+    isMarkerShown: false,
+    showInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {},
+    windowPosition: null,
+    mapClicked: false,
+  }
+
+  componentWillMount() {
+    const refs = {}
+
+    this.setState({
+      bounds: null,
+      center: {
+        lat: 40, lng: 45
+      },
+      markers: [],
+      onMapMounted: ref => {
+        refs.map = ref;
+      },
+      onBoundsChanged: () => {
+        this.setState({
+          bounds: refs.map.getBounds(),
+          center: refs.map.getCenter(),
+        })
+      },
+      onSearchBoxMounted: ref => {
+        refs.searchBox = ref;
+      },
+      onPlacesChanged: () => {
+        const places = refs.searchBox.getPlaces();
+        const bounds = new google.maps.LatLngBounds();
+
+        places.forEach(place => {
+          if (place.geometry.viewport) {
+            bounds.union(place.geometry.viewport)
+          } else {
+            bounds.extend(place.geometry.location)
+          }
+        });
+        const nextMarkers = places.map(place => ({
+          position: place.geometry.location,
+        }));
+        const nextCenter = _.get(nextMarkers, '0.position', this.state.center);
+
+        this.setState({
+          center: nextCenter,
+          markers: nextMarkers,
+        });
+        refs.map.fitBounds(bounds);
+      },
+    })
+  }
+
+  componentDidMount() {
+    this.delayedShowMarker()
+  }
+
+  delayedShowMarker = () => {
+    setTimeout(() => {
+      this.setState({ isMarkerShown: true })
+    }, 3000)
+  }
+
+  render() {
+    return (
+      <div>
+        <GoogleMap
+          ref={this.state.onMapMounted}
+          defaultZoom={2.2}
+          center={this.state.center}
+          onClick={this.props.handleClick}
+          onBoundsChanged={this.props.onBoundsChanged}
+          defaultOptions={{
+            // these following 7 options turn certain controls off see link below
+            streetViewControl: false,
+            scaleControl: false,
+            mapTypeControl: false,
+            panControl: false,
+            // zoomControl: false,
+            rotateControl: false,
+            fullscreenControl: false}}>
+              < SearchBox
+            ref={ this.state.onSearchBoxMounted }
+            bounds={ this.state.bounds }
+            controlPosition={ google.maps.ControlPosition.TOP_LEFT }
+            
+            onPlacesChanged={ this.state.onPlacesChanged } >
+            <input
+              type="text"
+              placeholder="Enter Site Location"
+              style={{
+                boxSizing: `border-box`,
+                border: `1px solid transparent`,
+                width: `80%`,
+                height: `6%`,
+                marginTop: `5%`,
+                marginLeft: '10%',
+                padding: `0 12px`,
+                borderRadius: `3px`,
+                boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                fontSize: `2vmin`,
+                outline: `none`,
+                textOverflow: `ellipses`,
+              }}
+            />
+          </SearchBox>
+              {/*  Display Markers with InfoWindows */}
+              {this.props.sitesByProject.map((site, index) => <SiteMarker key={index}
+                                        position={{ lat: Number(site.latitude), lng: Number(site.longitude)}}
+                                        selectSite= {this.props.selectSite}
+                                        index={index}
+                                    />)}
+
+                                    {/* End Markers Display */}
+        </GoogleMap>
+      </div>
+    );
+  }
+}
+const mapStateToProps = state => ({
+  state: state,
+  sitesByProject: state.sitesByProject,
+});
+
+export default connect(mapStateToProps)(withScriptjs(withGoogleMap(SitesMap)))
