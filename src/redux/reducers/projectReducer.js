f643fd77cb59be88a5f8833b0772d124a9f1e843
Nathan Salazar <nathan.salazar20@gmail.com>
2018-11-28 14:42:56 -0600

    clear out redux store on logout and addSite

--- src/redux/reducers/projectReducer.js
+++ src/redux/reducers/projectReducer.js
@@ -2,6 +2,8 @@ const project = (state = [], action) => {
     switch (action.type) {
       case 'SET_PROJECT':
         return action.payload;
+      case 'UNSET_USER':
+        return [];
       default:
         return state;
     }
--- src/redux/reducers/siteTypesReducer.js
+++ src/redux/reducers/siteTypesReducer.js
@@ -2,6 +2,10 @@ const siteTypesReducer = (state = [], action) => {
     switch (action.type) {
       case 'SET_SITE_TYPES':
         return action.payload;
+      case 'UNSET_USER':
+        return [];
+      case 'ADD_SITE_TO_PROJECT':
+        return [];
       default:
         return state;
     }
